1aba63faa382fcb82060738bad94700ae0a740fa
Matias-Smith1614 <matiassmith1614@gmail.com>
2025-01-05 01:22:41 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+
+// Definimos los datos de la experiencia
+const experiences = [
+    {
+        title: "Desarrollador Front-End",
+        company: "Tech Solutions",
+        duration: "Enero 2023 - Actualidad",
+        description: "Trabajo en el desarrollo de interfaces de usuario con React y diseño responsive."
+    },
+    {
+        title: "Desarrollador Web Junior",
+        company: "Web Creators",
+        duration: "Junio 2021 - Diciembre 2022",
+        description: "Desarrollé sitios web con HTML, CSS y JavaScript, y participé en proyectos con Node.js."
+    }
+];
+
